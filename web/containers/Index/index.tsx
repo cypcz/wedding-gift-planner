99e5c31e5aebd8d6b4dbdfbe0f6b179cacd635b1
Jakub <mr.bukaj@gmail.com>
2020-08-27 21:16:27 +0200

    Test again

--- web/containers/Index/index.tsx
+++ web/containers/Index/index.tsx
@@ -77,7 +77,7 @@ const Index = () => {
           <button onClick={handleLogout}>logout</button>
         )}
       </main>
-      <footer>footerrr</footer>
+      <footer>footerrrxxx</footer>
     </>
   );
 };
