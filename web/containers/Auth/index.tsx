23827e03ccc4ca979574ffd393ecc2e3654915d1
Jakub <mr.bukaj@gmail.com>
2020-08-30 19:32:52 +0200

    Fix build

--- web/containers/Auth/index.tsx
+++ web/containers/Auth/index.tsx
@@ -1,12 +1,10 @@
 //import firebase from "@utils/firebase";
-import { UserContext } from "@utils/userContext";
 import Link from "next/link";
-import { useContext } from "react";
 
 const Auth = () => {
-  const { user, loading, refetchUser } = useContext(UserContext);
+  //  const { user, loading, refetchUser } = useContext(UserContext);
 
-  if (loading) return <>loading...</>;
+  //  if (loading) return <>loading...</>;
 
   return (
     <div className="flex flex-col items-center">
